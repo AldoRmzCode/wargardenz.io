269d6844f6d28469542712550d070b646e6cf035
AldoRmzCode <aldoraga0209@gmail.com>
2022-12-01 23:14:23 -0600

    servidores sin cloudflare

--- src/GameScene.js
+++ src/GameScene.js
@@ -519,7 +519,7 @@ class GameScene extends Phaser.Scene {
 					doit = setTimeout(resize, 100);
 				  });
 				//go packet
-				var server = this.options.server == "eu1" ? "wss://srv.wargardenz.com" : this.options.server == "us2" ? "wss://srv.wargardenz.com" : "wss://srv.wargardenz.com";
+				var server = this.options.server == "eu1" ? "wss://wargardenz.com" : this.options.server == "us2" ? "wss://wargardenz.com" : "wss://wargardenz.com";
 				// server = undefined; // Enable for localhost/development
 				function isPrivateIP(ip) {
 					//remove port if present
--- src/TitleScene.js
+++ src/TitleScene.js
@@ -30,9 +30,9 @@ class TitleScene extends Phaser.Scene {
 
     const pingServers = (sethtml = true) => { 
       var servers = {
-       "us1": "https://srv.wargardenz.com",
-       "us2": "https://srv.wargardenz.com", 
-       "eu1": "https://srv.wargardenz.com"  
+       "us1": "https://wargardenz.com",
+       "us2": "https://wargardenz.com", 
+       "eu1": "https://wargardenz.com"  
       };
 
       var ping = (server) => {
