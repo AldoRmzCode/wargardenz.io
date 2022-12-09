6d3cd9d49a93b5fdaa8fc0f5cbd5302c5adbe436
AldoRmzCode <aldoraga0209@gmail.com>
2022-12-09 14:37:16 -0600

    rana evolution mejora

--- classes/evolutions/Knight.js
+++ classes/evolutions/Knight.js
@@ -3,29 +3,33 @@ class Knight extends Evolution {
     constructor() {
         super();
         this.name = "knight";
-        this.abilityDuration = 70000;
-        this.abilityCooldown = 40000;
+        this.abilityDuration = 8000;
+        this.abilityCooldown = 50000;
         this.subEvolutions = [];
     }
     default() {
         return {
-            maxHealth: 0.85,
-            health: 0.85,
-            speed: 1.2,
-            scale: 1.15,
-            power: 0.9,
-            resistance: 0.7,
-            damage: 1.1,
-            damageCooldown: 0.85,
-            healAmount: 0.8,
-            leech: 1.1,
+            maxHealth: 1.4,
+            health: 1.4,
+            speed: 0.7,
+            scale: 1.3,
+            power: 1,
+            resistance: 5.5,
+            damage: 1.6,
+            damageCooldown: 1.3,
+            healAmount: 1.5,
+            healWait: 2
         };
     }
     ability() {
         return {
-            leech: 1.2,
-            speed: 1.7,
+            healWait: 0,
+            healAmount: 1.5,
+            scale: 1.6,
+            power: 2.5,
+            damageCooldown: 0.7,
             resistance: 1,
+            speed: 1.5,
         };
     }
 }
--- classes/evolutions/Ratoncin.js
+++ classes/evolutions/Ratoncin.js
@@ -4,27 +4,29 @@ class Robot extends Evolution {
     constructor() {
         super();
         this.name = "ratoncin";
-        this.abilityDuration = 10000;
-        this.abilityCooldown = 60000;
+        this.abilityDuration = 70000;
+        this.abilityCooldown = 40000;
         this.subEvolutions = [];
     }
     default() {
         return {
-            speed: 1.1,
-            power: 1.1,
-            resistance: 0.9,
-            damage: 1,
-            damageCooldown: 1,
+            maxHealth: 0.85,
+            health: 0.85,
+            speed: 1.2,
+            scale: 1.15,
+            power: 0.9,
+            resistance: 0.7,
+            damage: 1.1,
+            damageCooldown: 0.85,
+            healAmount: 0.8,
+            leech: 1.1,
         };
     }
     ability() {
         return {
-            speed: 2,
-            power: 0.7,
-            resistance: 0.2,
-            damage: 1.5,
-            damageCooldown: 0.3,
-
+            leech: 1.2,
+            speed: 1.7,
+            resistance: 1,
         };
     }
 }
